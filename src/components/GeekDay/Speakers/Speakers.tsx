f4139245e6444006362fba5e41d7aa25a9d38c1c
Mert Gürer <mert.gurer@hotmail.com.tr>
2025-02-20 18:46:19 +0300

    update

--- src/components/GeekDay/Speakers/Speakers.tsx
+++ src/components/GeekDay/Speakers/Speakers.tsx
@@ -5,30 +5,30 @@ import SpeakerBox from "./SpeakerBox";
 import SpeakersList from "../../../assets/json/Speakers.json";
 
 const Speakers = () => {
-    return (
-        <section id="speakers" className="speakers-container">
-            <div className="speakers-header-box">
-                <p className="speakers-header">Bu seneki konuşmacılarımız</p>
-                <p className="speakers-info">
-                    Aynı anda üç salonda misafir ettiğimiz birbirinden değerli
-                    konuşmacılarımız
-                </p>
-            </div>
-            <div className="speakers-list">
-                {SpeakersList.map((speaker, index) => {
-                    return (
-                        <SpeakerBox
-                            key={index}
-                            name={speaker.name}
-                            subject={speaker.subject}
-                            company={speaker.company}
-                            image={speaker.image}
-                        />
-                    );
-                })}
-            </div>
-        </section>
-    );
+  return (
+    <section id="speakers" className="speakers-container">
+      <div className="speakers-header-box">
+        <p className="speakers-header">Bu seneki konuşmacılarımız</p>
+        <p className="speakers-info">
+          Aynı anda iki salonda misafir ettiğimiz birbirinden değerli
+          konuşmacılarımız
+        </p>
+      </div>
+      <div className="speakers-list">
+        {SpeakersList.map((speaker, index) => {
+          return (
+            <SpeakerBox
+              key={index}
+              name={speaker.name}
+              subject={speaker.subject}
+              company={speaker.company}
+              image={speaker.image}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Speakers;
